660392164f3290d9d41d0d27e6a794b6b985045c
alexsandro49 <alexsccpead@gmail.com>
2023-09-01 21:46:14 -0300

    Otimização no menu

--- EstoquePOO/src/App.java
+++ EstoquePOO/src/App.java
@@ -16,37 +16,24 @@ public class App {
 
             switch (opc) {
                 case "1": 
-                    System.out.print("DIGITE O NOME: ");
-                    String vendasNome = scan.nextLine();
-                    System.out.print("DIGITE A QUANTIDADE: ");
-                    int vendasQuantidade = scan.nextInt();
-                    estoque.removerProdutos(vendasNome, vendasQuantidade);
+                    String[] dadosVenda = dados('0');
+                    estoque.removerProdutos(dadosVenda[0], Integer.parseInt(dadosVenda[1]));
                     break;
                 case "2":
-                    System.out.print("DIGITE O NOME: ");
-                    String reporNome = scan.nextLine();
-                    System.out.print("DIGITE A QUANTIDADE: ");
-                    int reporQuantidade = scan.nextInt();
-                    estoque.adicionarProdutos(reporNome, reporQuantidade);
+                    String[] dadosRepor = dados('0');
+                    estoque.adicionarProdutos(dadosRepor[0], Integer.parseInt(dadosRepor[1]));
                     break;
                 case "3":
-                    System.out.print("NOME: ");
-                    String cadastrarNome = scan.nextLine();
-                    System.out.print("PREÇO: R$");
-                    float cadastrarPreco = scan.nextFloat();
-                    System.out.print("QUANTIDADE: ");
-                    int cadastrarQuantidade = scan.nextInt();
-                    scan.nextLine();
-                    System.out.print("GELADA (s/n): ");
-                    String gelada = scan.nextLine();
-                    if (gelada.toUpperCase().equals("S")) {
-                        Bebida bebida = new Bebida(cadastrarNome, cadastrarPreco, cadastrarQuantidade, LocalDate.now(), true);
+                    String[] dadosUsuario = dados('3');
+
+                    if (dadosUsuario[3].equals("S")) {
+                        Bebida bebida = new Bebida(dadosUsuario[0], Float.parseFloat(dadosUsuario[1]), Integer.parseInt(dadosUsuario[2]), LocalDate.now(), true);
                         estoque.cadastrarProduto(bebida);
-                    } else if (gelada.toUpperCase().equals("N")) {
-                        Bebida bebida = new Bebida(cadastrarNome, cadastrarPreco, cadastrarQuantidade, LocalDate.now(), false);
+                    } else if (dadosUsuario[3].equals("N")) {
+                        Bebida bebida = new Bebida(dadosUsuario[0], Float.parseFloat(dadosUsuario[1]), Integer.parseInt(dadosUsuario[2]), LocalDate.now(), false);
                         estoque.cadastrarProduto(bebida);
                     } else {
-                        System.out.println("ERRO!");
+                        System.out.println("\nERRO!");
                     }
                     break;
                 case "4": 
@@ -54,16 +41,52 @@ public class App {
                     System.out.println("\n============");
                     for (Map<String, String> produto : dados) {
                         System.out.println("NOME: " + produto.get("nome") + "\nQTD: " + produto.get("quantidade"));
+                        System.out.println("============");
                     }
-                    System.out.println("\n============");
+                    System.out.println();
                     break;
                 case "0":
-                    System.out.println("\nFINALIZANDO APLICAÇÃO!.\n");
+                    System.out.println("\nFINALIZANDO APLICAÇÃO!\n");
                     break;
                 default:
                     System.out.println("\nOPÇÃO INVÁLIDA! POR FAVOR, TENTE NOVAMENTE.\n");
                     break;
             }
         } while (!opc.equals("0"));
+    }
+
+    public static String[] dados(char opc) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("\nNOME: ");
+        String nome = scan.nextLine();
+
+        float preco = 0;
+        if (opc == '3') {
+            System.out.print("PREÇO: R$");
+            preco = scan.nextFloat();
+            scan.nextLine();
+        }
+
+        System.out.print("QUANTIDADE: ");
+        int quantidade = scan.nextInt();
+        scan.nextLine();
+
+        if (opc == '3') {
+            System.out.print("GELADO (s/n): ");
+            String gelada = scan.nextLine();
+            String[] dados = { nome.toUpperCase(), Float.toString(preco), Integer.toString(quantidade), "Z" };
+
+            if (gelada.toUpperCase().equals("S")) {
+                dados[3] = "S";
+            } else if (gelada.toUpperCase().equals("N")) {
+                dados[3] = "N";
+            }
+
+            return dados;
+        } else {
+            String[] dados = { nome, Integer.toString(quantidade) };
+            return dados;
+        }
     }
 }
